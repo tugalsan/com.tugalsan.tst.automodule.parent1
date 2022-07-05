ee702b4859d8c274fd0ee8ff31f9aced3a2f8ce0
tugalsan karabacak <tugalsan@gmail.com>
2022-07-05 17:04:14 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/tst/automodule/parent1/client/TGS_TstAutomoduleParent1.java
@@ -0,0 +1,10 @@
+package com.tugalsan.tst.automodule.parent1.client;
+
+import com.tugalsan.tst.automodule.child.client.*;
+
+public class TGS_TstAutomoduleParent1 {
+
+    public static void doSth() {
+        TGS_TstAutomoduleChild.doSth();
+    }
+}
